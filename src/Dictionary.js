4198f60d0b6abf52add6f8461897bc36daad75d0
hcutter <110829589+hcutter@users.noreply.github.com>
2023-09-22 11:48:04 -0400

    dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
-import "./Dictionary.css"
+import axios from "axios";
+import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
 
+    function handleResponse(response) {
+        console.log(response);
+    }
+
     function search(event) {
         event.preventDefault();
-        alert(`Searching for definition of ${keyword}`);
+        
+        let key = "6da73513tdfa9bf3a542b484aeb6odc0";
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
+        console.log(apiUrl);
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeywordChange(event) {
